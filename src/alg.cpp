ddcb8ec59aba5f453da0327b8fb0a5989cb5b76f
Daria <75723901+dariabo4kareva@users.noreply.github.com>
2021-03-23 17:15:14 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,6 +1,25 @@
 // Copyright 2021 NNTU-CS
 
 int cbinsearch(int *arr, int size, int value) {
-  //  поместить сюда реализацию алгоритма
-  return 0; // если ничего не найдено
+  int bgn = 0;
+  int end = size - 1;
+  int cnt = 0;
+
+  while (bgn < end) {
+      int k = (bgn+end) / 2;
+      if (arr[k] < value) {
+          bgn = k+1;
+      } else {
+          end = k;
+      }
+  }
+
+  if (arr[bgn] == value) {
+      while (arr[bgn] == value) {
+          bgn++;
+          cnt++;
+      }
+  }
+  
+  return cnt;
 }
